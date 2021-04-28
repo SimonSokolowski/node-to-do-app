d0d9821bf063c68283eade441626db18408e5078
simon <simon.sokolowski@outlook.com>
2021-04-28 15:34:52 +0100

    created working database using mogoDB atlas

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,6 +1,21 @@
 var express = require("express");
 var mongoose = require("mongoose");
 
+//connect to database
+mongoose.connect("mongodb+srv://todo:todo@cluster0.cy09m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
+
+//create schema
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+var Todo = mongoose.model("todo", todoSchema);
+
+var itemOne = Todo({item: "get flowers"}).save(function(err){
+    if (err) throw err;
+    console.log("item saved");
+});
+
 var data = [{item: "get milk"}, {item: "walk dog"}, {item: "do some coding"}];
 var urlencodedParser = express.urlencoded({extended: false});
 
