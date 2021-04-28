3f8ebd215d2d64d6ffc337b146bc916e5b74d412
simon <simon.sokolowski@outlook.com>
2021-04-28 14:59:03 +0100

    added ability to view, add, delete list items.

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var todoController = require("./controllers/todoController");
+var app = express();
+
+//set up template engine
+app.set("view engine", "ejs");
+
+//static files
+app.use(express.static("./public"));
+
+//fire controllers
+todoController(app);
+
+//Get website pages
+app.get("/", function(req, res){
+    res.render(home);
+});
+
+//listen to port
+app.listen(3000);
